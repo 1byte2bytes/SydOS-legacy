ce174af9cca5b84880db0a69e388895af7e42b9a
Sydney Erickson <sydneyerickson@icloud.com>
2017-02-18 21:59:10 -0800

    SYDOS-1 Create 4MB stack for C programs

--- src/loader.s
+++ src/loader.s
@@ -4,6 +4,8 @@ MAGIC_NUMBER equ 0x1BADB002
 FLAGS        equ 0x0
 CHECKSUM     equ -MAGIC_NUMBER
 
+KERNEL_STACK_SIZE equ 4096
+
 section .text:
 align 4
     dd MAGIC_NUMBER
@@ -12,5 +14,11 @@ align 4
 
 loader:
     mov eax, 0xCAFEBABE
+    mov esp, kernel_stack + KERNEL_STACK_SIZE
 .loop:
     jmp .loop
+
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE
