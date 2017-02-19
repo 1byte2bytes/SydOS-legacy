c64480899a014345972603135476b0a00916f29a
Sydney Erickson <sydneyerickson@icloud.com>
2017-02-19 13:10:33 -0800

    SYDOS-2 Created a function to write letters to the screen

--- src/drivers/framebuffer/fb_main.c
+++ src/drivers/framebuffer/fb_main.c
@@ -1,2 +1,14 @@
 char * fb_text_buffer = (char *)0x000B8000;
 
+/** fb_write:
+ *  Writes a charecter to the screen at the specified location
+ *
+ *  @param i  The location in the framebuffer
+ *  @param c  The charecter to write
+ *  @param fg The foreground color in hex
+ *  @param bg The background color in hex
+ */
+void fb_write(unsigned int i, char c, unsigned char fg, unsigned char bg) {
+    fb_text_buffer[i] = c;
+    fb_text_buffer[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+}
---
+++ src/drivers/framebuffer/fb_main.h
@@ -0,0 +1,1 @@
+extern int fb_main(unsigned int i, char c, unsigned char fg, unsigned char bg)
